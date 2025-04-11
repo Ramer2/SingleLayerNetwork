bee89fd9d039e7f8f9d315512c6ba7be42e35399
s30422 <s30422@pjwstk.edu.pl>
2025-04-11 12:49:30 +0200

    Corrected ugly code

--- src/TrainingSet.java
+++ src/TrainingSet.java
@@ -25,37 +25,12 @@ public class TrainingSet {
                     default -> "";
                 };
 
+                String text = parts[1].toLowerCase().replaceAll("[^a-z]", "");
+
                 double[] components = new double[26];
-                char[] letter = parts[1].toCharArray();
+                char[] letter = text.toCharArray();
                 for (char c : letter) {
-                    switch (c) {
-                        case 'a': components[0]++; break;
-                        case 'b': components[1]++; break;
-                        case 'c': components[2]++; break;
-                        case 'd': components[3]++; break;
-                        case 'e': components[4]++; break;
-                        case 'f': components[5]++; break;
-                        case 'g': components[6]++; break;
-                        case 'h': components[7]++; break;
-                        case 'i': components[8]++; break;
-                        case 'j': components[9]++; break;
-                        case 'k': components[10]++; break;
-                        case 'l': components[11]++; break;
-                        case 'm': components[12]++; break;
-                        case 'n': components[13]++; break;
-                        case 'o': components[14]++; break;
-                        case 'p': components[15]++; break;
-                        case 'q': components[16]++; break;
-                        case 'r': components[17]++; break;
-                        case 's': components[18]++; break;
-                        case 't': components[19]++; break;
-                        case 'u': components[20]++; break;
-                        case 'v': components[21]++; break;
-                        case 'w': components[22]++; break;
-                        case 'x': components[23]++; break;
-                        case 'y': components[24]++; break;
-                        case 'z': components[25]++; break;
-                    }
+                    components[c - 'a']++;
                 }
 
                 trainingVectors.add(new Vector(components, lang));
